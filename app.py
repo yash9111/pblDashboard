bde3fd51feffbb1b4260fa472ffb63affa27fcff
Yash pathak <assush698@gmail.com>
2023-09-20 18:57:41 +0530

    initial

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+import firebase_admin
+from firebase_admin import auth
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("confedential/private.json")
+firebase_admin.initialize_app(cred)
+
+
+
+app = FastAPI()
+
+@app.post("/login")
+async def login(user_id: str, password: str):
+    try:
+        # Authenticate with Firebase
+        user = auth.get_user_by_email(user_id)
+
+        print(user)
+
+        return {"message": "Login successful", "user_id": user_id}
+    except auth.UserNotFoundError:
+        raise HTTPException(status_code=401, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
